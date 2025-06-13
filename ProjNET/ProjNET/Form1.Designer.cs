73660e03251a287e280d14eb9603f3236cd798b7
mixa381 <151820554+mixa381@users.noreply.github.com>
2025-06-13 19:58:28 +0200

    few

---
+++ ProjNET/ProjNET/Form1.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace ProjNET
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.fromlabel = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelTo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fromlabel
+            // 
+            this.fromlabel.AutoSize = true;
+            this.fromlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fromlabel.Location = new System.Drawing.Point(22, 71);
+            this.fromlabel.Margin = new System.Windows.Forms.Padding(2, 20, 2, 20);
+            this.fromlabel.Name = "fromlabel";
+            this.fromlabel.Size = new System.Drawing.Size(60, 24);
+            this.fromlabel.TabIndex = 0;
+            this.fromlabel.Text = "From:";
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            this.chart1.BorderlineWidth = 3;
+            chartArea1.AxisX.MajorGrid.Enabled = false;
+            chartArea1.AxisY.MajorGrid.Enabled = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.chart1.Location = new System.Drawing.Point(20, 20);
+            this.chart1.Margin = new System.Windows.Forms.Padding(0);
+            this.chart1.Name = "chart1";
+            this.chart1.Padding = new System.Windows.Forms.Padding(20);
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SeaGreen;
+            series1.ChartArea = "ChartArea1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(477, 460);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            this.chart1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.chart1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(20, 20);
+            this.button1.Margin = new System.Windows.Forms.Padding(20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AED",
+            "ALL",
+            "AMD",
+            "ARS",
+            "AUD",
+            "AWG",
+            "BAM",
+            "BGN",
+            "BRL",
+            "BYN",
+            "CAD",
+            "CHF",
+            "CNY",
+            "COP",
+            "CZK",
+            "DKK",
+            "EEK",
+            "EUR",
+            "EUR",
+            "GBP",
+            "GBP",
+            "HKD",
+            "HRK",
+            "HUF",
+            "IDR",
+            "INR",
+            "ISK",
+            "JPY",
+            "KRW",
+            "KZT",
+            "LTL",
+            "LVL",
+            "MDL",
+            "MKD",
+            "MXN",
+            "MYR",
+            "NOK",
+            "NZD",
+            "PLN",
+            "RON",
+            "RSD",
+            "RUB",
+            "RUB",
+            "SAR",
+            "SEK",
+            "SEK",
+            "SGD",
+            "TRY",
+            "TRY",
+            "UAH",
+            "USD",
+            "XOF",
+            "XPF",
+            "ZAR"});
+            this.comboBox1.Location = new System.Drawing.Point(20, 103);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(219, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox2.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "USD",
+            "EUR",
+            "JPY",
+            "GBP",
+            "AUD",
+            "CAD",
+            "CHF",
+            "CNY",
+            "SEK",
+            "NZD",
+            "INR",
+            "BRL",
+            "MXN",
+            "ZAR",
+            "HKD",
+            "KRW",
+            "SGD",
+            "RUB",
+            "TRY",
+            "IDR",
+            "MYR",
+            "SAR",
+            "AED",
+            "COP",
+            "ARS",
+            "ALL",
+            "AMD",
+            "AWG",
+            "BAM",
+            "BGN",
+            "BYN",
+            "CZK",
+            "DKK",
+            "EEK",
+            "EUR",
+            "GBP",
+            "HRK",
+            "HUF",
+            "ISK",
+            "KZT",
+            "LTL",
+            "LVL",
+            "MDL",
+            "MKD",
+            "NOK",
+            "PLN",
+            "RON",
+            "RUB",
+            "SEK",
+            "TRY",
+            "UAH",
+            "XOF",
+            "XPF",
+            "RSD"});
+            this.comboBox2.Location = new System.Drawing.Point(20, 164);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(20);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(219, 21);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(261, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(20);
+            this.panel1.Size = new System.Drawing.Size(539, 500);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.labelTo);
+            this.panel2.Controls.Add(this.comboBox2);
+            this.panel2.Controls.Add(this.comboBox1);
+            this.panel2.Controls.Add(this.fromlabel);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Padding = new System.Windows.Forms.Padding(20);
+            this.panel2.Size = new System.Drawing.Size(261, 500);
+            this.panel2.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(20, 208);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(219, 270);
+            this.panel3.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 24);
+            this.label1.TabIndex = 0;
+            // 
+            // labelTo
+            // 
+            this.labelTo.AutoSize = true;
+            this.labelTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTo.Location = new System.Drawing.Point(22, 135);
+            this.labelTo.Margin = new System.Windows.Forms.Padding(2, 20, 2, 20);
+            this.labelTo.Name = "labelTo";
+            this.labelTo.Size = new System.Drawing.Size(38, 24);
+            this.labelTo.TabIndex = 5;
+            this.labelTo.Text = "To:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MinimumSize = new System.Drawing.Size(800, 500);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label fromlabel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelTo;
+    }
+}
+
---
+++ ProjNET/ProjNET/Form1.cs
@@ -0,0 +1,230 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+namespace ProjNET
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string GenerateApiUrl()
+        {
+            // Uzmi izabrane vrednosti iz ListBox-a
+            string baseCurrency = comboBox1.SelectedItem?.ToString();
+            string targetCurrency = comboBox2.SelectedItem?.ToString();
+            // Dobijanje datuma za start i end
+            DateTime today = DateTime.Today;
+            DateTime startDate = today.AddMonths(-6);  // Start je 6 meseci unazad
+            string startDateStr = startDate.ToString("yyyy-MM-dd");
+            string endDateStr = today.ToString("yyyy-MM-dd");  // End je danas
+            
+            // Formiranje URL-a
+            string url = $"https://api.fxratesapi.com/timeseries?start_date={startDateStr}&end_date={endDateStr}&currencies={targetCurrency}&base={baseCurrency}&places=5";
+
+            return url;
+        }
+        private async Task<Dictionary<string, decimal>> GetLast6MonthsExchangeRatesAsync()
+        {
+            string url = GenerateApiUrl();
+            using (var client = new HttpClient())
+            {
+                try
+                {
+
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    string json = await response.Content.ReadAsStringAsync();
+                    JObject data = JObject.Parse(json);
+
+                    var rates = data["rates"] as JObject;
+                    if (rates == null || rates.Count == 0)
+                        return null;
+
+                    // Parsiranje i sortiranje datuma
+                    var parsedRates = new Dictionary<string, decimal>();
+
+                    foreach (var item in rates.Properties())
+                    {
+                        string date = item.Name;
+                        var rateToken = item.Value[comboBox2.SelectedItem?.ToString()];
+                        if (rateToken != null)
+                        {
+                            parsedRates[date] = rateToken.Value<decimal>();
+                        }
+                    }
+                    DateTime sixMonthsAgo = DateTime.Today.AddMonths(-6);
+
+                    var last6Months = parsedRates
+                        .Where(kvp => DateTime.Parse(kvp.Key) >= sixMonthsAgo)
+                        .OrderBy(kvp => DateTime.Parse(kvp.Key))
+                        .Reverse()
+                        .ToDictionary(k => k.Key, v => v.Value);
+
+                    return last6Months;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+
+        private void AdjustChartZoom()
+        {
+            // Dobijanje svih X i Y vrednosti sa tačaka
+            var xValues = chart1.Series[0].Points.Select(p => p.XValue).ToList();
+            var yValues = chart1.Series[0].Points.Select(p => p.YValues[0]).ToList();
+
+            // Ako nema podataka, ništa ne radimo
+            if (xValues.Count == 0 || yValues.Count == 0)
+                return;
+
+            double xMin = xValues.Min();
+            double xMax = xValues.Max();
+
+            DateTime minDate = DateTime.FromOADate(xMin);
+            DateTime maxDate = DateTime.FromOADate(xMax);
+
+            DateTime sixMonthsAgo = DateTime.Today.AddMonths(-6);
+
+            if (minDate > sixMonthsAgo)
+            {
+                minDate = sixMonthsAgo;
+            }
+
+            double yMin = yValues.Min();
+            double yMax = yValues.Max();
+            double yRange = yMax - yMin;
+            double yMargin = yRange * 0.05;
+
+            
+            chart1.ChartAreas[0].AxisY.Minimum = yMin - yMargin;
+            chart1.ChartAreas[0].AxisY.Maximum = yMax + yMargin;
+
+            chart1.ChartAreas[0].AxisX.Minimum = sixMonthsAgo.ToOADate(); 
+
+            
+            chart1.ChartAreas[0].AxisX.IntervalType = DateTimeIntervalType.Days;
+            chart1.ChartAreas[0].AxisX.Interval = 30;
+
+            // Prilagodite format prikaza na X osi
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "dd.MM.yy";
+            chart1.ChartAreas[0].AxisX.MajorGrid.Enabled = false;
+            chart1.ChartAreas[0].AxisY.MajorGrid.Enabled = false;
+
+            // Osvežimo graf da se nove postavke primene
+            chart1.Invalidate();
+        }
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Loading...";
+            
+            var rates = await GetLast6MonthsExchangeRatesAsync();
+            if (rates == null || rates.Count == 0)
+            {
+                label1.Text = "Greska pri ucitavanju.";
+                return;
+            }
+
+            // Očistite prethodne podatke sa grafa
+            chart1.Series.Clear();
+            chart1.ChartAreas.Clear();
+            chart1.ChartAreas.Add(new ChartArea("MainArea"));
+
+            // Dodavanje novih podataka u seriju
+            var series = new Series("USD to RSD")
+            {
+                ChartType = SeriesChartType.Line,
+                BorderWidth = 3
+            };
+            int dayCounter = 0;
+            
+            foreach (var kvp in rates)
+            {
+                DateTime date = DateTime.Parse(kvp.Key);
+
+                // Dodajemo samo svaku 5. tačku
+                if (dayCounter % 5 == 0)
+                {
+                    series.Points.AddXY(date, kvp.Value);
+                }
+
+                dayCounter++;
+            }
+
+            chart1.Series.Add(series);
+
+            AdjustChartZoom(); // Automatsko prilagođavanje
+
+            // Postavite format za X osu
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "dd.MM.yyyy";
+            chart1.ChartAreas[0].AxisX.LabelStyle.ForeColor = Color.Gray;
+            chart1.ChartAreas[0].AxisY.LabelStyle.ForeColor =   Color.Gray;
+
+            if (series.Points.Count > 0)
+            {
+                double lastYValue = series.Points.First().YValues[0];
+                label1.Text = $"1{comboBox1.SelectedItem.ToString()} = {lastYValue:F4}{comboBox2.SelectedItem.ToString()}";
+            }
+            
+        }
+
+        private void chart1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Detekcija da li je mišem prešao preko tačke na grafu
+            HitTestResult result = chart1.HitTest(e.X, e.Y);
+
+            // Ako je korisnik prešao preko serije podataka (DataPoint), prikaži tooltip
+            if (result.ChartElementType == ChartElementType.DataPoint)
+            {
+                var dataPoint = chart1.Series[0].Points[result.PointIndex];
+                double pointXValue = dataPoint.XValue;
+
+                DateTime pointDate = DateTime.FromOADate(pointXValue);
+                double pointValue = dataPoint.YValues[0];
+
+                toolTip1.SetToolTip(chart1, $"Datum: {pointDate.ToShortDateString()} - Vrednost: {pointValue:F2}");
+
+                // Dodaj tačku na graf na mestu gde je pokazivač
+                AddDotOnChart(result.ChartElementType, result.PointIndex);
+            }
+        }
+
+        private void AddDotOnChart(ChartElementType chartElementType, int pointIndex)
+        {
+            // Ako su podaci u grafu i postoji serija, dodajemo marker samo na tački
+            if (chartElementType == ChartElementType.DataPoint)
+            {
+                // Ukloni prethodnu marker seriju ako postoji
+                if (chart1.Series.IndexOf("Marker") >= 0)
+                {
+                    chart1.Series.Remove(chart1.Series["Marker"]);
+                }
+
+                var markerSeries = chart1.Series.Add("Marker");
+                markerSeries.ChartType = SeriesChartType.Point;
+                markerSeries.MarkerStyle = MarkerStyle.Circle; 
+                markerSeries.MarkerSize = 10;
+                markerSeries.Color = Color.FromArgb(150, 250, 150);
+
+                // Dodajemo marker na tačno poziciju DataPoint-a
+                var dataPoint = chart1.Series[0].Points[pointIndex];
+                markerSeries.Points.AddXY(dataPoint.XValue, dataPoint.YValues[0]);
+            }
+        }
+    }
+}
